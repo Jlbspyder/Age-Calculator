847a670179c9c5da4b5d264725a12f4b88fca57b
Debo <gbadebodebo24@gmail.com>
2023-04-23 00:56:24 +0100

    edit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 
 const Footer = () => {
   return (
-  <div class="attribution">
+  <div className="attribution">
     Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
     Coded by <a href="https://github.com/Jlbspyder" target="_blank">Jlbspyder</a>.
   </div> 
