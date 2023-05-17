c68d90181300432ad31dea69d77f2f0474d77d51
Jlbspyder <111382307+Jlbspyder@users.noreply.github.com>
2023-05-18 00:30:46 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
   return (
   <div className="attribution">
     Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-    Coded by <a href="https://github.com/Jlbspyder" target="_blank">Gbadebo</a>.
+    Coded by <a href="https://github.com/Jlbspyder" target="_blank">Jlbspyder</a>.
   </div> 
   )
 }
