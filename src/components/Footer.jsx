c1f9f23721c1119f927db045dce7e2cac155e26e
Jlbspyder <111382307+Jlbspyder@users.noreply.github.com>
2023-06-19 02:38:13 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@
 const Footer = () => {
   return (
   <div className="attribution">
-    Birthday wishes from <a href="https://github.com/Jlbspyder" target="_blank">DEBO</a>.
+    Greetings from <a href="https://github.com/Jlbspyder" target="_blank">DEBO</a>.
   </div> 
   )
 }
